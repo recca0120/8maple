3358e9d8c8feb5087d5be9cb691a020c658be1bc
recca0120 <recca0120@gmail.com>
2023-10-18 17:05:10 +0800

    refactor

--- crawlers.py
+++ crawlers.py
@@ -13,17 +13,36 @@ class Crawler(ABC):
     def __init__(self, http: Http = None):
         self._http = Http() if http is None else http
 
-    @abstractmethod
-    async def pages(self, name: str, url: str, start: Union[int, str, None] = None, end: Union[int, str, None] = None):
-        pass
-
     async def _get_html(self, url):
         response = await self._http.get(url)
 
         return response.decode('utf-8')
 
+    @abstractmethod
+    def get_episodes(self, soup: BeautifulSoup, url: str):
+        pass
+
+    async def pages(self, name: str, url: str, start: Union[int, str, None] = None, end: Union[int, str, None] = None):
+        parsed = urlparse(url)
+        base_url = '%s://%s' % (parsed.scheme, parsed.netloc)
+
+        soup = BeautifulSoup(await self._get_html(url), 'html.parser')
+        for link in self.get_episodes(soup, url):
+            url = f'{base_url}{link["href"]}'
+            _m3u8 = self.__get_m3u8(await self._get_html(url))
+
+            matched = re.search(r'第([\w\\.]+)集', link.text)
+            episode = matched.group(1) if matched is not None else link.text.strip()
+
+            try:
+                episode = int(episode)
+                if self.__allowed(episode, start, end):
+                    yield Page(name, episode, url, _m3u8)
+            except ValueError:
+                yield Page(name, episode, url, _m3u8)
+
     @staticmethod
-    def _allowed(episode: int, start, end):
+    def __allowed(episode: int, start, end):
         if start is not None and start > episode:
             return False
 
@@ -33,56 +52,24 @@ class Crawler(ABC):
         return True
 
     @staticmethod
-    def _get_m3u8(html: str) -> str:
+    def __get_m3u8(html: str) -> str:
         url = re.search(r'\"https:.+?\.m3u8\"', html).group(0)
 
         return json.loads('{"url": %s}' % url)['url']
 
 
 class Bowang(Crawler):
-
-    async def pages(self, name: str, url: str, start: Union[int, str, None] = None, end: Union[int, str, None] = None):
-        parsed = urlparse(url)
-        base_url = '%s://%s' % (parsed.scheme, parsed.netloc)
-
-        soup = BeautifulSoup(await self._get_html(url), 'html.parser')
-        for link in soup.select(".play-tab-list.active .module-play-list-link"):
-            url = f'{base_url}{link["href"]}'
-            _m3u8 = self._get_m3u8(await self._get_html(url))
-
-            matched = re.search(r'第([\w\\.]+)集', link.text)
-            episode = matched.group(1) if matched is not None else link.text.strip()
-
-            try:
-                episode = int(episode)
-                if self._allowed(episode, start, end):
-                    yield Page(name, episode, url, _m3u8)
-            except ValueError:
-                yield Page(name, episode, url, _m3u8)
+    def get_episodes(self, soup: BeautifulSoup, url: str):
+        return soup.select(".play-tab-list.active .module-play-list-link")
 
 
 class Gimy(Crawler):
-    async def pages(self, name: str, url: str, start: Union[int, str, None] = None, end: Union[int, str, None] = None):
+    def get_episodes(self, soup: BeautifulSoup, url: str):
         parsed = urlparse(url)
-        base_url = '%s://%s' % (parsed.scheme, parsed.netloc)
-
         path = parsed.path
         episode_prefix = (path[0:path.rfind('-')])
 
-        soup = BeautifulSoup(await self._get_html(url), 'html.parser')
-        for link in soup.select(f'.playlist[class*="activeplayer"] li a[href^="{episode_prefix}"]'):
-            url = f'{base_url}{link["href"]}'
-            _m3u8 = self._get_m3u8(await self._get_html(url))
-
-            matched = re.search(r'第([\w\\.]+)集', link.text)
-            episode = matched.group(1) if matched is not None else link.text.strip()
-
-            try:
-                episode = int(episode)
-                if self._allowed(episode, start, end):
-                    yield Page(name, episode, url, _m3u8)
-            except ValueError:
-                yield Page(name, episode, url, _m3u8)
+        return soup.select(f'.playlist[class*="activeplayer"] li a[href^="{episode_prefix}"]')
 
 
 class Page:
@@ -101,21 +88,21 @@ class Page:
 
 
 class Factory(object):
+    crawlers = {
+        'bowang': Bowang,
+        'gimy': Gimy
+    }
+
     def __init__(self, http: Http = None):
         self.__http = Http() if http is None else http
 
     def create(self, url: str):
-        crawlers = {
-            'bowang': Bowang,
-            'gimy': Gimy
-        }
-
-        return crawlers.get(self.parse_name(url))(self.__http)
+        return self.crawlers.get(self.parse_name(url))(self.__http)
 
     @staticmethod
     def parse_name(url: str):
         domain = urlparse(url).netloc
-        if re.search(u'gimy', domain):
+        if 'gimy' in domain:
             return 'gimy'
 
         return 'bowang'
